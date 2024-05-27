254ce6313e33a7f1590f64cd38946259ea35c610
Dzzjio <giojio14@gmail.com>
2024-05-27 16:18:29 +0400

    make project slider responsive

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -221,6 +221,20 @@ const AboutPageSlider = () => {
         navigation
         slidesPerView={3}
         loop={true}
+        breakpoints={{
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          }
+        }}
       >
 
       {projects.map((project, index) => (
