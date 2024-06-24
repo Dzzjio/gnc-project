3c95c7603814d02b8f5bf7930e9a669614707d02
dzzjio <giojio14@gmail.com>
2024-06-24 13:25:10 +0400

    add show/hide header on scroll

--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -1,17 +1,36 @@
-import { Outlet } from 'react-router-dom'
-import Header from '../Layout/Header/Header'
-import Footer from '../Layout/Footer/Footer'
-import HeaderTop from '../Layout/Header/HeaderTop'
+import { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Layout/Header/Header';
+import Footer from '../Layout/Footer/Footer';
+import HeaderTop from '../Layout/Header/HeaderTop';
 
 const Root = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [showHeader, setShowHeader] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    setShowHeader(scrollPosition > currentScrollPos || currentScrollPos < 10);
+    setScrollPosition(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollPosition]);
+
   return (
     <div className="app">
-      <HeaderTop />
-      <Header />
+      <div className={`header-container ${showHeader ? 'show' : 'hide'}`}>
+        <HeaderTop />
+        <Header />
+      </div>
       <Outlet />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
